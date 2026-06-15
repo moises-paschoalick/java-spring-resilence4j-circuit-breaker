c4576f43cd07d5ce33b62eff2d0504976d75b7cf
Moisés Guilherme <moisescomp@gmail.com>
2026-06-15 06:27:41 -0300

    feat: add redis service

---
+++ src/main/java/com/paschoalick/publication/service/RedisService.java
@@ -0,0 +1,27 @@
+package com.paschoalick.publication.service;
+
+import com.paschoalick.publication.domain.Comment;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RedisService {
+
+    private static final String KEY = "Comment";
+
+
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    public void save(List<Comment> comments, String id) {
+        redisTemplate.opsForHash().put(KEY, id, comments);
+    }
+
+    public List<Comment> findById(String id) {
+        return (List<Comment>) redisTemplate.opsForHash().get(KEY, id);
+    }
+
+}
